9b8dd1ff055dfc3d810d7cdeb4c9425eae2477f8
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-11 09:48:24 +0100

    intro typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+// Creiamo il file .ts
+// apriamo il terminale
+// e scriviamo tsc index.ts per convertire il file in js
+//e colleghiamo il nuovo file che ha creato vs code all'HTML
+// se nel terminale scriviamo tsc -w index.js la pagina si aggiornerà automaticamente
+// ctrl  + c per fermare il watching (nel terminale)
